e6706aac8ec0e617eb632e95fccd367939694e4c
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-08-02 19:14:37 +0530

    React Slider done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import img1 from "./assets/img1.jpeg";
+import img2 from "./assets/img2.webp";
+import img3 from "./assets/img3.webp";
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const mod = (n, m) => {
+    let result = n % m;
+
+    // Return a positive value
+    return result >= 0 ? result : result + m;
+  };
+
+  const cards = [
+    {
+      id: "1",
+      image: img1,
+    },
+    {
+      id: "2",
+      image: img2,
+    },
+    {
+      id: "3",
+      image: img3,
+    },
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIndex((index + 1) % cards.length);
+      console.log(index);
+    }, 3000);
+  }, [index]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="carousel">
+        {cards.map((item, i) => {
+          const indexLeft = mod(index - 1, cards.length);
+          const indexRight = mod(index + 1, cards.length);
+
+          let className = "card";
+
+          if (i === index) {
+            className = "card card--active";
+          } else if (i === indexRight) {
+            className = "card card--right";
+          } else if (i === indexLeft) {
+            className = "card card--left";
+          } else className = "card";
+
+          return (
+            <img
+              key={item.id}
+              className={className}
+              src={item.image}
+              alt="Comic"
+            ></img>
+          );
+        })}
+      </div>
     </div>
   );
 }
